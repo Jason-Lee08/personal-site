1a0c8703fb69f4ff2bf0d8ce3ca4c33c06b7f1aa
Jason Lee <jason_lee@berkeley.edu>
2024-10-01 13:19:21 -0700

    yes

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -4,31 +4,31 @@ import { Link } from 'react-router-dom';
 import Main from '../layouts/Main';
 
 const Index = () => (
-    <Main
-      description={
-        'Jason Lee\'s personal website. UC Berkeley Engineering student, '
-        + 'undergraduate AI researcher, and tech-enthusiast.'
-      }
-    >
-      <article className="post" id="index">
-        <header>
-          <div className="title">
-            <h2><Link to="/">Welcome to my website!</Link></h2>
-            <p>
-              A beautiful, responsive, statically-generated,
-              react application written with modern Javascript.
-            </p>
-          </div>
-        </header>
-        <p>
-          Please feel free to read more <Link to="/about">about me</Link>,
-          or you can check out my {' '}
-          <Link to="/resume">resume</Link>, {' '}
-          <Link to="/projects">projects</Link>, {' '}
-          or <Link to="/contact">contact</Link> me.
-        </p>
-      </article>
-    </Main>
+  <Main
+    description={
+      'Jason Lee\'s personal website. UC Berkeley Engineering student, '
+      + 'undergraduate AI researcher, and tech-enthusiast.'
+    }
+  >
+    <article className="post" id="index">
+      <header>
+        <div className="title">
+          <h2><Link to="/">Welcome to my website!</Link></h2>
+          <p>
+            A beautiful, responsive, statically-generated,
+            react application written with modern Javascript.
+          </p>
+        </div>
+      </header>
+      <p>
+        Please feel free to read more <Link to="/about">about me</Link>,
+        or you can check out my {' '}
+        <Link to="/resume">resume</Link>, {' '}
+        <Link to="/projects">projects</Link>, {' '}
+        or <Link to="/contact">contact</Link> me.
+      </p>
+    </article>
+  </Main>
 );
 
 export default Index;
